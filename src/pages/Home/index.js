4908ae7599afbcced7d6d2320806de60e1dbd56f
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-06-08 01:57:50 -0300

    React Native Insider
    Aula 01

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import StatusBarPage from '../../components/StatusBarPage';
+import Menu from '../../components/Menu';
+
+export default function Home() {
+   return(
+      <LinearGradient
+         colors={['#1ddbb9', '#132742']}
+         style={{ flex:1, justifyContent: 'center'}}
+      >
+
+         <StatusBarPage 
+            barStyle="light-content"
+            backgroundColor="#1ddbb9"
+         />
+
+         <Menu />
+
+         <Text>Página Home</Text>
+      </LinearGradient>
+   )
+}
---
+++ src/pages/MyLinks/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import StatusBarPage from '../../components/StatusBarPage';
+
+export default function MyLinks() {
+   return(
+      <View>
+         <StatusBarPage 
+         barStyle="light-content"
+         backgroundColor="#132742"
+         />
+         <Text>Página Links</Text>
+      </View>
+   )
+}
