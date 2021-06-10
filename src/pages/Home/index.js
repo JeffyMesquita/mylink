28c65aefee2a8fa67a205d5570d92701f5b33bf1
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-06-10 02:30:05 -0300

    Aula 02
    UX/UI design básico,
    Styled Components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,56 +1,91 @@
-import React from 'react';
+import React , { useState } from 'react';
+import { TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Modal } from 'react-native';
 
 import { LinearGradient } from 'expo-linear-gradient';
 import StatusBarPage from '../../components/StatusBarPage';
 import Menu from '../../components/Menu';
+import ModalLink from '../../components/ModalLink';
 
 import { Feather } from '@expo/vector-icons';
 import { ContainerLogo, Logo, ContainerContent, Title, SubTitle, ContainerInput, BoxIcon, Input, ButtonLink, ButtonLinkText } from './styles'
 
 export default function Home() {
+
+   const [input, setInput] = useState('');
+   const [modalVisible, setModalVisible] = useState(false);
+
+   function handleShortLink() {
+      //alert('URL DIGITADA: '+ input)
+
+      setModalVisible(true);
+   }
+
    return(
-      <LinearGradient
-         colors={['#1ddbb9', '#132742']}
-         style={{ flex:1, justifyContent: 'center'}}
-      >
+      <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+         <LinearGradient
+            colors={['#1ddbb9', '#132742']}
+            style={{ flex:1, justifyContent: 'center'}}
+         >
 
-         <StatusBarPage 
-            barStyle="light-content"
-            backgroundColor="#1ddbb9"
-         />
-
-         <Menu />
-
-         <ContainerLogo>
-            <Logo 
-               source={require('../../assets/Logo.png')} 
-               resizeMode="contain"
+            <StatusBarPage 
+               barStyle="light-content"
+               backgroundColor="#1ddbb9"
             />
-         </ContainerLogo>
 
-         <ContainerContent>
-            <Title>SujeitoLink</Title>
-            <SubTitle>Cole seu link para encurtar</SubTitle>
+            <Menu />
 
-            <ContainerInput>
-               <BoxIcon>
-                  <Feather 
-                     name="link"
-                     size={22}
-                     color="#FFF"
+            <KeyboardAvoidingView
+               behavior={ Platform.OS === 'android' ? 'padding' : 'position'}
+               enabled={true}
+            >
+               <ContainerLogo>
+                  <Logo 
+                     source={require('../../assets/Logo.png')} 
+                     resizeMode="contain"
                   />
-               </BoxIcon>
-               <Input 
-                  placeholder="Cole seu link aqui..."
-                  placeholderTextColor="white"
-               />
-            </ContainerInput>
+               </ContainerLogo>
 
-            <ButtonLink> 
-               <ButtonLinkText>Gerar Link</ButtonLinkText>
-            </ButtonLink>
+               <ContainerContent>
+                  <Title>SujeitoLink</Title>
+                  <SubTitle>Cole seu link para encurtar</SubTitle>
 
-         </ContainerContent>
-      </LinearGradient>
+                  <ContainerInput>
+                     <BoxIcon>
+                        <Feather 
+                           name="link"
+                           size={22}
+                           color="#FFF"
+                        />
+                     </BoxIcon>
+                     <Input 
+                        placeholder="Cole seu link aqui..."
+                        placeholderTextColor="white"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        keyboardType="url"
+                        value={input}
+                        onChangeText={ (text) => setInput(text) }
+                     />
+                  </ContainerInput>
+
+                  <ButtonLink
+                     onPress={ handleShortLink }
+                  > 
+                     <ButtonLinkText>Gerar Link</ButtonLinkText>
+                  </ButtonLink>
+
+               </ContainerContent>
+            </KeyboardAvoidingView>
+
+            <Modal
+               visible={modalVisible}
+               transparent
+               animationType="slide"
+            >
+               <ModalLink onClose={ () => setModalVisible(false)}/>
+            </Modal>
+
+         </LinearGradient>
+      </TouchableWithoutFeedback>
    )
 }
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Menu from '../../components/Menu';
 import StatusBarPage from '../../components/StatusBarPage';
+import { Container, Title, ListLinks } from './styles';
+import ListItem from '../../components/ListItem';
 
 export default function MyLinks() {
    return(
-      <View>
-         <StatusBarPage 
-         barStyle="light-content"
-         backgroundColor="#132742"
-         />
-         <Text>Página Links</Text>
-      </View>
+      <Container>
+         <View>
+            <StatusBarPage 
+            barStyle="light-content"
+            backgroundColor="#132742"
+            />
+
+            <Menu />
+
+            <Title>Meus Links</Title>
+
+            <ListLinks 
+               data={[ {id: 1, link: 'teste.com'}, {id: 2, link: 'teste.com'} ]}
+               keyExtractor={ (item) => String(item.id)}
+               renderItem={ ({item}) => <ListItem data={item}/>}
+               contentContainerStyle={{ paddingBottom: 20}}
+               showsVerticalScrollIndicator={false}
+            />
+         </View>
+      </Container>
    )
 }
